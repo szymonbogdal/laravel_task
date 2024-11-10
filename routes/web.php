2ffdc87b222579206632c3e0dc7a0854eaebc1e5
Szymon Bogdał <bogdal.symon@gmail.com>
2024-11-10 13:45:17 +0100

    web.php add pets routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PetController;
 
 Route::get('/', function () {
-    return view('welcome');
+  return redirect()->route('pets.index', ['status' => 'available']);
 });
+
+Route::get('/pets', function () {
+  return redirect()->route('pets.index', ['status' => 'available']);
+});
+
+Route::get('pets/status/{status?}', [PetController::class, 'index'])->name("pets.index");
+Route::resource('pets', PetController::class)->except(['index']);
